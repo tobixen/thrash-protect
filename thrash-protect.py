5ebd06fda374026f3b02e992a7f31a22a96c340f
Tobias Brox <tobias@redpill-linpro.com>
2013-12-19 12:34:14 +0100

    bugfixing

--- thrash-protect.py
+++ thrash-protect.py
@@ -20,7 +20,7 @@ try:
 except NameError:
   FileNotFoundError=IOError
 
-__version__ = "0.5.2.3"
+__version__ = "0.5.2.4"
 __author__ = "Tobias Brox"
 __copyright__ = "Copyright 2013, Tobias Brox"
 __license__ = "GPL"
