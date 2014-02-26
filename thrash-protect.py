e3f7daf2ba0701f7aed7ea076d462ad71020b144
Tobias Brox <tobias@redpill-linpro.com>
2014-02-26 12:17:18 +0100

    bumping the version number

--- thrash-protect.py
+++ thrash-protect.py
@@ -20,7 +20,7 @@ try:
 except NameError:
   FileNotFoundError=IOError
 
-__version__ = "0.7.1"
+__version__ = "0.8"
 __author__ = "Tobias Brox"
 __copyright__ = "Copyright 2013, Tobias Brox"
 __license__ = "GPL"
