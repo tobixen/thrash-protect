e49ec39aade80ec0d1ab883a7b87797765da8ce2
Tobias Brox <t-git@tobixen.no>
2014-11-18 20:42:52 +0100

    thrash bot provided by a customer

---
+++ tests/thrash-bot2.py
@@ -0,0 +1,18 @@
+import os
+os.system('dd if=/dev/zero of=/tmp/bigfile bs=1M count=1')
+
+
+
+import array
+
+def alloc(megs):
+    res = array.array('c')
+    for i in range(megs):
+        res.fromfile(open('/tmp/bigfile', 'rb'), 1048576)
+    return res
+
+i=0
+while True:
+    i+=1
+    print("alloc %d" % i)
+    alloc(i)
