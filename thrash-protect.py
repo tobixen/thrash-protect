dfcb2e617dcc051fc46bd73c5d8b6fca8e6ead96
Tobias Brox <tobias@redpill-linpro.com>
2014-11-21 00:24:58 +0100

    pushed version number to 0.9.0

--- thrash-protect.py
+++ thrash-protect.py
@@ -20,9 +20,9 @@ try:
 except NameError:
     FileNotFoundError=IOError
 
-__version__ = "0.9.0-devel"
+__version__ = "0.9.0"
 __author__ = "Tobias Brox"
-__copyright__ = "Copyright 2013, Tobias Brox"
+__copyright__ = "Copyright 2013-2014, Tobias Brox"
 __license__ = "GPL"
 __maintainer__ = "Tobias Brox"
 __email__ = "tobias@redpill-linpro.com"
