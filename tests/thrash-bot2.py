77353977602f169e1ec6ca7bec65892cf936e77f
Tobias Brox <tobias@redpill-linpro.com>
2014-12-18 18:27:06 +0100

    improving the thrash bot

--- tests/thrash-bot2.py
+++ tests/thrash-bot2.py
@@ -1,18 +1,25 @@
 import os
-os.system('dd if=/dev/zero of=/tmp/bigfile bs=1M count=1')
-
-
-
 import array
+import argparse
 
-def alloc(megs):
-    res = array.array('c')
+
+def alloc(megs, filename):
+    res = array.array('b')
     for i in range(megs):
         res.fromfile(open('/tmp/bigfile', 'rb'), 1048576)
     return res
 
-i=6700
-while True:
-    i+=1
-    print("alloc %d" % i)
-    alloc(i)
+fn = '/tmp/bigfile.%s' % os.getpid()
+parser = argparse.ArgumentParser()
+parser.add_argument("--alloc-start", help="start by allocating this amount of memory (should be a bit less than what's available)", default=3000, type=int)
+args = parser.parse_args()
+i = args.alloc_start
+try:
+    os.system('dd if=/dev/zero of=%s bs=1M count=1' % fn)
+    while True:
+        i+=1
+        print("alloc %d" % i)
+        alloc(i, fn)
+finally:
+    os.unlink(fn)
+    
