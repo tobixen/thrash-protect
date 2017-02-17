0720984182157edc111823fbeec644752b8c03be
Tobias Brox <tobias@redpill-linpro.com>
2017-02-17 13:38:46 +0100

    releasing 0.11.5

--- thrash-protect.py
+++ thrash-protect.py
@@ -20,7 +20,7 @@ try:
 except NameError:
     FileNotFoundError=IOError
 
-__version__ = "0.11.4"
+__version__ = "0.11.5"
 __author__ = "Tobias Brox"
 __copyright__ = "Copyright 2013-2016, Tobias Brox"
 __license__ = "GPL"
